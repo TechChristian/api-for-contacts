573bbebe9434d7331c9b9819cad14bf265661c32
Christian <christian@MacBook-Air-de-Christian.local>
2026-04-07 08:42:19 -0300

    first commit

---
+++ src/test/java/br/com/christian/contacts/ContactsApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.christian.contacts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+/*
+@SpringBootTest
+class ContactsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
