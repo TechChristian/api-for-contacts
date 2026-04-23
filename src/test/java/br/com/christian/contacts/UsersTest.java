4eee43988f72fb19b2f96b262c435a8b119db3d5
Christian <christianx111.ls@gmail.com>
2026-04-23 12:31:55 -0300

    Ajustes de scripts sql e testes para validar informacoes de usuarios criados com sucesso, esperando um status 201.

---
+++ src/test/java/br/com/christian/contacts/UsersTest.java
@@ -0,0 +1,42 @@
+package br.com.christian.contacts;
+
+import br.com.christian.contacts.dto.request.UserRequestDto;
+import br.com.christian.contacts.dto.response.UserResponseDto;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+@Sql(scripts = "/sql/users/users-insert.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(scripts = "/sql/users/users-delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class UsersTest {
+    @Autowired
+    WebTestClient testClient;
+
+    @Test
+    // Test -> Create user with valid information
+    public void createUser_WithValidInformation_ReturnCode201(){
+        UserResponseDto responseBody = testClient
+                .post()
+                .uri("v1/users")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserRequestDto("chris@example.com", "christian", "123456789"))
+                .exchange()
+                .expectStatus().isCreated()
+                .expectBody(UserResponseDto.class)
+                .returnResult()
+                .getResponseBody();
+
+        Assertions.assertThat(responseBody).isNotNull();
+        Assertions.assertThat(responseBody.id()).isNotNull();
+        Assertions.assertThat(responseBody.email()).isEqualTo("chris@example.com");
+        Assertions.assertThat(responseBody.username()).isEqualTo("christian");
+        Assertions.assertThat(responseBody.roles().getRole().isEmpty());
+    }
+}
--- src/test/resources/sql/users/users-insert.sql
+++ src/test/resources/sql/users/users-insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO USERS (email, username, password, role)
-VALUES ('christian@example.com', 'christian', '123456789', 'USER');
+INSERT INTO USERS (id, email, username, password, role)
+VALUES (RANDOM_UUID(),'christian@example.com', 'christian', '123456789', 'USER');
 
-INSERT INTO USERS (email, username, password, role)
-VALUES ('jon@example.com', 'jon', '123456789', 'USER');
+INSERT INTO USERS (id,email, username, password, role)
+VALUES (RANDOM_UUID(),'jon@example.com', 'jon', '123456789', 'USER');
 
-INSERT INTO USERS (email, username, password, role)
-VALUES ('ben@example.com', 'ben', '123456789', 'USER');
+INSERT INTO USERS (id,email, username, password, role)
+VALUES (RANDOM_UUID(),'ben@example.com', 'ben', '123456789', 'USER');
